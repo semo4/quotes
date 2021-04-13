218c87ef9118fa50942edf13406ef0fd3a3366a3
semo4 <fotboy1788@hotmail.com>
2021-04-13 17:46:54 +0300

    add class 08

---
+++ app/src/main/java/quotes/Book.java
@@ -0,0 +1,31 @@
+package quotes;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Book {
+    String author;
+    String text;
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, text);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return  Objects.equals(author, book.author) && Objects.equals(text, book.text);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
